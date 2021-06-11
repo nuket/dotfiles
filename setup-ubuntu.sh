fa49e18dcddf28bd24275eb46eda0af1519582c8
Max Vilimpoc <max@vilimpoc.org>
2021-06-11 18:42:53 +0200

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -u
-set -x
+set -ux
 
 # Raspberry Pi 3
 
@@ -66,17 +65,3 @@ main_() {
     # Leave cron stuff for now
     # anacron at cron
 }
-
-# The following packages were automatically installed and are no longer required:
-#   cups-server-common gir1.2-dbusmenu-glib-0.4 gir1.2-dee-1.0 gir1.2-unity-5.0 hplip-data libavahi-core7 libhpmud0 libimagequant0
-#   libmbim-glib4 libmbim-proxy libqmi-glib5 libqmi-proxy libsane-hpaio printer-driver-postscript-hp python3-debconf python3-debian
-#   python3-olefile python3-pexpect python3-pil python3-ptyprocess python3-renderpm python3-reportlab python3-reportlab-accel
-#   ssl-cert usb-modeswitch usb-modeswitch-data
-# Use 'sudo apt autoremove' to remove them.
-# The following packages will be REMOVED:
-#   avahi-daemon avahi-utils bluez-cups cups cups-browsed cups-core-drivers cups-daemon firefox hplip irqbalance kerneloops
-#   libnss-mdns modemmanager popularity-contest printer-driver-hpcups printer-driver-splix switcheroo-control ubuntu-desktop
-#   ubuntu-desktop-minimal ubuntu-release-upgrader-gtk ubuntu-standard unattended-upgrades update-manager update-notifier
-#   update-notifier-common whoopsie
-# 0 upgraded, 0 newly installed, 26 to remove and 0 not upgraded.
-# After this operation, 240 MB disk space will be freed.
